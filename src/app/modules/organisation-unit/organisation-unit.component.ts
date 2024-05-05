ea97f07af25becc5896059b9a67e2b6c9a5bf4c0
Milton <miltonisaiah@gmail.com>
2024-05-05 11:13:18 +0300

    validation rules added

--- src/app/modules/organisation-unit/organisation-unit.component.ts
+++ src/app/modules/organisation-unit/organisation-unit.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-organisation-units',
+    selector: 'app-organisation-unit',
     templateUrl: './organisation-unit.component.html',
     styleUrl: './organisation-unit.component.css',
     standalone: true
