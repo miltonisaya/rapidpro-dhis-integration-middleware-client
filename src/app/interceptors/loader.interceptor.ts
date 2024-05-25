e9f1a7042a2bb04ede2ed917d3d1582930d124a4
Milton <miltonisaiah@gmail.com>
2024-05-25 16:32:52 +0300

    loader interceptor created

--- src/app/interceptors/loader.interceptor.ts
+++ src/app/interceptors/loader.interceptor.ts
@@ -1,28 +1,15 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError, finalize} from 'rxjs/operators';
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {LoginDialogComponent} from "../modules/login-dialog/login-dialog.component";
-import {NotifierService} from "../modules/notification/notifier.service";
-import {AuthService} from "../auth/auth.service";
-import {LoaderService} from "../modules/loader/loader.service";
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from "@angular/core";
+import { LoaderService } from "../modules/loader/loader.service";
+import { finalize } from "rxjs/operators";
 
-@Injectable()
-export class LoaderInterceptor implements HttpInterceptor {
-  constructor(
-    private loaderService: LoaderService
-  ) {
-  }
+export const LoaderInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn) => {
+  const loaderService = inject(LoaderService);
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loaderService.isLoading.next(true);
-    return next.handle(request).pipe(
-      finalize(
-        () => {
-          this.loaderService.isLoading.next(false);
-        }
-      )
-    );
-  }
-}
+  loaderService.isLoading.next(true);
+  return next(request).pipe(
+    finalize(() => {
+      loaderService.isLoading.next(false);
+    })
+  );
+};
