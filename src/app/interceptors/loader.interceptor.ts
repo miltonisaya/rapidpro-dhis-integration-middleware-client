41b70a544da3012539051e228879e02248a6a6a4
Milton <miltonisaiah@gmail.com>
2024-05-25 11:20:04 +0300

    loader interceptor added

---
+++ src/app/interceptors/loader.interceptor.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, finalize} from 'rxjs/operators';
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {LoginDialogComponent} from "../modules/login-dialog/login-dialog.component";
+import {NotifierService} from "../modules/notification/notifier.service";
+import {AuthService} from "../auth/auth.service";
+import {LoaderService} from "../modules/loader/loader.service";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private loaderService: LoaderService
+  ) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.isLoading.next(true);
+    return next.handle(request).pipe(
+      finalize(
+        () => {
+          this.loaderService.isLoading.next(false);
+        }
+      )
+    );
+  }
+}
