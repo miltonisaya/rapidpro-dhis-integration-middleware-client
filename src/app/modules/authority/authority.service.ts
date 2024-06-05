e3a86817b0fe9b8b016e48a8b8e44503ce0ed397
Milton <miltonisaiah@gmail.com>
2024-06-05 21:18:00 +0300

    find authorities by role created

--- src/app/modules/authority/authority.service.ts
+++ src/app/modules/authority/authority.service.ts
@@ -1,18 +1,32 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {RoleAuthorityApiResponse} from "../role/types/RoleAuthorityApiResponse";
+import {F} from "@angular/cdk/keycodes";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorityService {
   private baseUrl = 'http://localhost:8081/api/v1';
+  form: FormGroup = new FormGroup({
+    roleUuid: new FormControl('',Validators.required),
+    authorities: new FormArray([])
+  });
 
   constructor(private http: HttpClient) {
   }
 
-  // Read
-  get(params: { page: number; size: number; sort: string }): Observable<any> {
-    return this.http.get(`${this.baseUrl}/authorities?page=${params.page}&size=${params.size}&sort=${params.sort}`);
+  initializeFormGroup() {
+    return this.form.patchValue({
+      roleUuid: '',
+      authorityUuid: ''
+    });
+  }
+
+  findByRole(uuid: string): Observable<any> {
+    let requestUrl = `${this.baseUrl}/authorities/role/` + uuid;
+    return this.http.get(requestUrl);
   }
 }
