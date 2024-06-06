ed14f160d2980b7da888877b0142abaf566c445e
Milton <miltonisaiah@gmail.com>
2024-06-06 23:52:58 +0300

    routes corrected for root path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
-import { Routes } from "@angular/router";
-import { DashboardComponent } from "./modules/dashboard/dashboard.component";
-import { ContactComponent } from "./modules/contact/contact.component";
-import { FlowComponent } from "./modules/flow/flow.component";
-import { DataElementComponent } from "./modules/data-element/data-element.component";
-import { ProgramComponent } from "./modules/program/program.component";
-import { UserComponent } from "./modules/user/user.component";
-import { DefaultComponent } from "./layouts/default/default.component";
-import { LoginComponent } from "./layouts/login/login.component";
-import { RoleComponent } from "./modules/role/role.component";
-import { OrganisationUnitComponent } from "./modules/organisation-unit/organisation-unit.component";
-import { AuthGuard } from "./auth/auth.guard";
-import { MenuGroupComponent } from "./modules/menu-group/menu-group.component";
-import { MenuItemComponent } from "./modules/menu-item/menu-item.component";
+import {Routes} from "@angular/router";
+import {DashboardComponent} from "./modules/dashboard/dashboard.component";
+import {ContactComponent} from "./modules/contact/contact.component";
+import {FlowComponent} from "./modules/flow/flow.component";
+import {DataElementComponent} from "./modules/data-element/data-element.component";
+import {ProgramComponent} from "./modules/program/program.component";
+import {UserComponent} from "./modules/user/user.component";
+import {DefaultComponent} from "./layouts/default/default.component";
+import {LoginComponent} from "./layouts/login/login.component";
+import {RoleComponent} from "./modules/role/role.component";
+import {OrganisationUnitComponent} from "./modules/organisation-unit/organisation-unit.component";
+import {AuthGuard} from "./auth/auth.guard";
+import {MenuGroupComponent} from "./modules/menu-group/menu-group.component";
+import {MenuItemComponent} from "./modules/menu-item/menu-item.component";
 
 export const APP_ROUTES: Routes = [
   {
@@ -67,20 +67,12 @@ export const APP_ROUTES: Routes = [
         path: "menu-items",
         component: MenuItemComponent,
         canActivate: [AuthGuard]
-      }
-    ]
+      },
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+    ],
   },
   {
     path: "login",
     component: LoginComponent
-  },
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard'
   }
 ];
