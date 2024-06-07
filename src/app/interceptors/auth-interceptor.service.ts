02c69f30fa405e34aeed6b51b5c91730580344f3
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-07 17:09:07 +0300

    fix: refactor ui

--- src/app/interceptors/auth-interceptor.service.ts
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -1,50 +1,61 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
-import {catchError} from 'rxjs/operators';
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {AuthService} from "../auth/auth.service";
-import {LoginDialogComponent} from "../modules/login-dialog/login-dialog.component";
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AuthService } from '../auth/auth.service';
+import { LoginDialogComponent } from '../modules/login-dialog/login-dialog.component';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor(
     public auth: AuthService,
     public router: Router,
-    public dialog: MatDialog
-  ) {
-  }
+    public dialog: MatDialog,
+  ) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.auth.getToken()}`
-      }
+        Authorization: `Bearer ${this.auth.getToken()}`,
+      },
     });
 
     return next.handle(request).pipe(
-      catchError(response => {
+      catchError((response) => {
         if (response.status === 401) {
-          localStorage.setItem("CURRENT_ROUTE", JSON.stringify(this.router.url));
+          localStorage.setItem(
+            'CURRENT_ROUTE',
+            JSON.stringify(this.router.url),
+          );
           this.openLoginDialog();
-          console.log("Response unauthorized =>", response);
+          console.log('Response unauthorized =>', response);
           // this.router.navigate(["/login"]);
           return next.handle(request);
         }
         return throwError(response);
-      })
-    )
+      }),
+    );
   }
 
   openLoginDialog(): void {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    this.dialog.open(LoginDialogComponent, dialogConfig)
-      .afterClosed().subscribe(() => {
-      console.log("Dialog closed ...")
-    });
+    this.dialog
+      .open(LoginDialogComponent, dialogConfig)
+      .afterClosed()
+      .subscribe(() => {
+        console.log('Dialog closed ...');
+      });
   }
 }
--- src/app/interceptors/loader.interceptor.ts
+++ src/app/interceptors/loader.interceptor.ts
@@ -1,15 +1,22 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { inject } from "@angular/core";
-import { LoaderService } from "../modules/loader/loader.service";
-import { finalize } from "rxjs/operators";
+import {
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { LoaderService } from '../modules/loader/loader.service';
+import { finalize } from 'rxjs/operators';
 
-export const LoaderInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn) => {
+export const LoaderInterceptor: HttpInterceptorFn = (
+  request: HttpRequest<any>,
+  next: HttpHandlerFn,
+) => {
   const loaderService = inject(LoaderService);
 
   loaderService.isLoading.next(true);
   return next(request).pipe(
     finalize(() => {
       loaderService.isLoading.next(false);
-    })
+    }),
   );
 };
