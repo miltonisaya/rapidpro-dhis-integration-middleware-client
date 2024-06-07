01549e752743e41620c41e99602fa626b4b38f7b
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-07 16:52:36 +0300

    fix: code format

--- src/app/components/dialog-component.ts
+++ src/app/components/dialog-component.ts
@@ -9,11 +9,16 @@ import {
   SimpleChanges,
   TemplateRef,
 } from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef,} from '@angular/material/dialog';
-import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIcon, MatIconModule} from '@angular/material/icon';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dialog',
@@ -39,8 +44,7 @@ export class DialogComponent implements OnChanges {
 
   private dialogRef!: MatDialogRef<any>;
 
-  constructor(private dialog: MatDialog) {
-  }
+  constructor(private dialog: MatDialog) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['open'] && this.open) {
@@ -136,8 +140,7 @@ export class DialogContentComponent {
   constructor(
     public dialogRef: MatDialogRef<DialogContentComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) {
-  }
+  ) {}
 
   onCancelClick(): void {
     this.dialogRef.close(false);
