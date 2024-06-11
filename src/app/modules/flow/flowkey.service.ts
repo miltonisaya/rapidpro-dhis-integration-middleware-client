7777983415da7f1dc4c7fc74999bef18ad609aa5
Milton <miltonisaiah@gmail.com>
2024-06-11 10:16:31 +0300

    api endpoint corrected

--- src/app/modules/flow/flowkey.service.ts
+++ src/app/modules/flow/flowkey.service.ts
@@ -4,9 +4,13 @@ import {Observable, of} from 'rxjs';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {environment} from '../../../environments/environment';
 
+export const BASE_URL: string = environment.baseURL;
+export const RESOURCE_URL: string = 'api/v1/keys';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class FlowKeyService {
   form: FormGroup = new FormGroup({
     id: new FormControl(''),
@@ -14,35 +18,28 @@ export class FlowKeyService {
     rapidProFlowId: new FormControl('', Validators.required)
   });
 
-  private baseUrl: string = environment.baseURL;
+  private API_ENDPOINT = `${BASE_URL}/${RESOURCE_URL}`;
 
   constructor(private http: HttpClient) {
   }
 
   getKeysByFlowUuid(uuid: string): Observable<any> {
-    let requestUrl = `${this.baseUrl}/api/v1/get-rapid-pro-flow-keys-by-flow-id/${uuid}`;
+    let requestUrl = `${this.API_ENDPOINT}/get-rapid-pro-flow-keys-by-flow-id/${uuid}`;
     return this.http.get<any>(requestUrl);
   }
 
   updateFlowKey(flowKey: any): Observable<any> {
-    let requestUrl = `${this.baseUrl}/api/v1/flows/${flowKey.id}`;
+    let requestUrl = `${this.API_ENDPOINT}/flow/${flowKey.id}`;
     return this.http.put<any>(requestUrl, flowKey);
   }
 
   mapDataElement(data: any): Observable<any> {
-    let requestUrl = `${this.baseUrl}/api/v1/map-data-element`;
+    let requestUrl = `${this.API_ENDPOINT}/map-data-element`;
     return this.http.put<any>(requestUrl, data);
   }
 
   mapDataElementsWithCategory(data: any): Observable<any> {
-    let requestUrl: string = `${this.baseUrl}/api/v1/map-data-element-with-category/${data.categoryUuid}`;
+    let requestUrl: string = `${this.API_ENDPOINT}/map-data-element-with-category/${data.categoryUuid}`;
     return this.http.put<any>(requestUrl, data);
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(`${operation} failed:`, error);  // log to console
-      return of(result as T);  // Let the app keep running by returning a safe result.
-    };
   }
 }
